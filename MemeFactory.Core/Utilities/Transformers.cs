b8410141b694ff47e962011abf9abc820da05e4c
ZeroAsh <admin@remiliascarlet.com>
2025-03-13 01:11:23 +0800

    enhance: update image rotation method

--- MemeFactory.Core/Utilities/Transformers.cs
+++ MemeFactory.Core/Utilities/Transformers.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using MemeFactory.Core.Processing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
 namespace MemeFactory.Core.Utilities;
@@ -12,12 +14,18 @@ public static class Transformers
     {
         var deg = 360f / circleTimes;
         var allFrames = await frames.ToListAsync(cancellationToken);
-        var total = Algorithms.Lcm(allFrames.Count, circleTimes) / allFrames.Count - 1;
-
-        foreach (var frame in allFrames.Loop(total))
+        var total = Enumerable.Range(circleTimes - 2, 4)
+            .Select(c =>  Algorithms.Lcm(allFrames.Count, c) / allFrames.Count)
+            .Min() - 1;
+        var baseSize = allFrames[0].Image.Size;
+        foreach (var frame in allFrames.Loop(total)) using (frame)
         {
-            frame.Image.Mutate((ctx) => ctx.Rotate(deg * frame.Sequence));
-            yield return frame;
+            frame.Image.Mutate((ctx) => ctx.Rotate(deg * frame.Sequence - 1));
+            var newFrame = new Image<Rgba32>(baseSize.Width, baseSize.Height);
+            var x = (baseSize.Width - frame.Image.Width) / 2;
+            var y = (baseSize.Height - frame.Image.Height) / 2;
+            newFrame.Mutate((ctx) => ctx.DrawImage(frame.Image, new Point(x, y), 1f));
+            yield return frame with { Image = newFrame };
         }
     }
 }
